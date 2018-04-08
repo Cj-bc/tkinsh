dd972331a8890070d46c0face4dadfcd73802c7e
Cj-bc <cj-bc@y-modify.org>
2018-04-08 23:28:48 +0900

    Add: projinstall> Which helps install tkinsh into other's project

---
+++ projinstall.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+#
+# tkinsh.projinstall -- install tkinsh on your project
+#
+# copyright (c) 2018 Cj-bc
+#
+# This software is released under MIT License.
+# http://opensource.org/license/mit-license.php
+
+
+mv tkinsh.sh ../
+mv lib ../.tkinshlib
+cd ../ && rmdir tkinsh
